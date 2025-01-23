d8f382bb07bbcf816e9cecc8cf97764142ce80ca
Tobias <lesertobias3@gmail.com>
2025-01-23 16:27:35 +0100

    reviewing commentary code and add copyright all pages

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database.db');
 
-// Créer une table pour les utilisateurs (id, username, mot de passe, solde)
+
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS users (
